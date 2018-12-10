9f090cc6eded65435c7c883662ce59e696be85ea
github username <github email address>
2018-12-10 18:26:01 +0000

    Completed lab

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word[2] = "$"
+  end
+  array
+end
+
+def find_a(array)
+  array.select do |word|
+    word[0] == "a"
+  end
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  new_array = []
+
+  array.each do |word|
+    if word !="feet"
+      new_array << "#{word}s"
+    else
+      new_array << word
+    end
+  end
+  return new_array
+end
